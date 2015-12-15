c808fc6cdee9bb4c764b3b6db2118aeb78fb5ae4
alena <alena@debian>
2015-12-15 18:08:14 +0300

    news

--- app/views/home/news.blade.php
+++ app/views/home/news.blade.php
@@ -25,19 +25,7 @@
                 <h1>{{$row->name}}</h1>
                 {{$row->text}}
                 <br><a href="javascript:history.back(1)" >назад</a>
-                <br><br>
-                <div class="col-xs-12 text-right share-block">
-                <script type="text/javascript">(function() {
-                  if (window.pluso)if (typeof window.pluso.start == "function") return;
-                  if (window.ifpluso==undefined) { window.ifpluso = 1;
-                    var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
-                    s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
-                    s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
-                    var h=d[g]('body')[0];
-                    h.appendChild(s);
-                  }})();</script>
-                <span>Поделиться:</span> <div class="pluso" data-background="transparent" data-options="small,square,line,horizontal,nocounter,theme=02" data-services="vkontakte,odnoklassniki,facebook,twitter,google" data-user="1132713077"></div>
-                </div>                
+
             @endif
 
             @if(isset($posts)&&count($posts)>0)
@@ -52,12 +40,13 @@
                         </div>
                         <div class="col-xs-12 col-sm-6 part part-title">
                             <a href="{{$type->type.'/'.$post->slug}}"><h2>{{ $post->name }}</h2></a>
+
                         </div>
                     </div>
 
                     <div id="parts-{{$post->id}}" class="hidden-parts">{{ $post->preview }}</div>
                     <div class="text-right">
-                        <p>{{ HTML::link( $type->type.'/'.$post->slug, 'далее' ) }} </p>
+                        <p>{{ HTML::link( $type->type.'/'.$post->slug, 'читать далее' ) }} </p>
                     </div>
                 </div>    
                 <hr>
