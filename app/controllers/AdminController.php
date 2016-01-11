760da8c8102fde51d53215e78fbba3a669052b14
alena <alena@debian>
2016-01-11 16:45:53 +0300

    add small image

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -204,12 +204,13 @@ class AdminController extends BaseController {
             if(!empty($image)) {
                 $image_name = explode("/", $image);
                 if(!empty($image_name[2])) {
-var_dump($post, $image_name);
-die();
+//var_dump($post, $image_name);
+//die();
                     $image_name = $image_name[2];
                     if (!is_dir('upload/image/small/')) {
                         mkdir('upload/image/small/', 0777, true);
                     }
+
                     Image::make($image)->resize('300', null, function ($constraint) {
                         $constraint->aspectRatio();
                     })->save('upload/image/small/' . $image_name);
